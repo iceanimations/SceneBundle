73d1d864f8abb3b91741761d9dc9490df1a2c781
Talha Ahmed <talha.ahmed@gmail.com>
2015-01-28 18:58:50 +0500

    getPrefferedPool

--- src/_deadline.py
+++ src/_deadline.py
@@ -1,50 +1,69 @@
-import os
-import os.path as op
+#import os
+#import os.path as op
+
+
+from collections import OrderedDict
 
 import ideadline as dl
 reload(dl)
 
-#import ideadline.maya as dlm
-#reload(dlm)
+import ideadline.maya as dlm
+reload(dlm)
 
-rs_pools = {'rs'+(str(idx)):r'd:\shared\lb\rs'+(str(idx)) for idx in
-        range(10) }
+variables = ['bundle_base', 'poolidx', 'project', 'episode', 'sequence', 'shot']
+num_pools = 3
+bundle_base = r'\\hp-001\drive%(poolidx)d'
+output_loc = r'\\ice-lac\%(project)s\02_production\EP_%(episode)02d\SQ_%(sequence)02d\SH_%(shot)02d'
+bundle_loc = r'%(bundle_base)s\%(project)s\EP%(episode)02d\SQ%(sequence)03d\SH%(shot)03d'
 
-def mkdir(path):
-    if not op.exists(op.dirname(path)):
-        mkdir(op.dirname(path))
-    os.mkdir(path)
 
-for value in rs_pools.values():
-    try:
-        mkdir(value)
-    except:
-        pass
+rs_pools = OrderedDict([('rs'+(str(idx)), r'\shared\lb\rs'+(str(idx)))
+        for idx in range(1, num_pools+1) ])
 
-def createJobs(pool=None):
+
+def getPreferredPool():
+    poolframes = getFramesPendingOnPools(getRedshiftPools())
+    return min(enumerate(poolframes.values()), key=lambda x:poolframes[x[1]])
+
+
+
+def createJobs(pool=None, outputPath=None, projectPath=None, sceneFile=None,
+        jobName=None):
     submitter = dlm.DeadlineMayaSubmitter()
     if pool:
         submitter.pool=pool
+    if outputPath:
+        submitter.outputPath = outputPath
+    if projectPath:
+        submitter.projectPath = projectPath
+    if sceneFile:
+        submitter.sceneFile = sceneFile
+    if jobName:
+        submitter.jobName = jobName
     return submitter.createJobs()
+
 
 def getRedshiftPools():
     return [pool for pool in dl.pools() if pool in rs_pools]
+
 
 def getFramesPendingInJob(job):
     totalFrames = len(job["Frames"].split(","))
     doneRatio = float(job["QueuedChunks"])/int(job["TaskCount"])
     return int(totalFrames * doneRatio)
 
+
 def getFramesPendingOnPools(pools,
         statuses = ["Queued", "Active", "Suspended"]):
     jobs = dl.getJobs()
     jobs = dl.filterItems(jobs,
             [("Status", status) for status in statuses] )
-    frames = {}
+    frames = OrderedDict()
     for pool in pools:
         pooljobs = dl.filterItems(jobs,[("PoolOverride", pool)])
         frames[pool]= sum([getFramesPendingInJob(job) for job in pooljobs])
     return frames
+
 
 if __name__ == '__main__':
     print getFramesPendingOnPools(dl.pools())
