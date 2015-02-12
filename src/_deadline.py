43ca043ec541b58355110bfb59781c9a347774c2
Talha Ahmed <talha.ahmed@gmail.com>
2015-02-12 15:43:13 +0500

    implemented with config (untested)

--- src/_deadline.py
+++ src/_deadline.py
@@ -37,6 +37,7 @@ if not config:
 
     config['output_loc'] = r'\\ice-lac\Storage\Projects\external\%(project)s\02_production\%(episode)s\%(sequence)s\%(shot)s'
     config['bundle_loc'] = r'%(bundle_base)s\%(project)s\%(episode)s\%(sequence)s\%(shot)s'
+    config['job_name'] = '%(project)s_%(episode)s_%(sequence)s_%(shot)s - %(name)'
     config['illegal_layer_names'] = ['.*depth.*']
     config['illegal_camera_names'] = []
     config['pools'] = {
@@ -49,7 +50,7 @@ if not config:
 
     config['overrides'] = [
             { # first override
-                'Conditions':[[ 'Renderer', 'redshift' ]],
+                'conditions':[[ 'Renderer', 'redshift' ]],
                 'match_all': True,
                 'settings': {
                     'pools': {
@@ -61,7 +62,7 @@ if not config:
                     'pool_selection': 'min_frames_pending' }
             },
             { # second override
-                'Conditions':[[ 'renderer', 'arnold' ]],
+                'conditions':[[ 'renderer', 'arnold' ]],
                 'match_all': True,
                 'settings': {
                     'pools': {
@@ -75,31 +76,202 @@ if not config:
             }
     ]
 
-#all_pools = config['pools'].copy
-#for key, value in [override['settings']['pools'].items() for override in
-        #config['overrides'] if override['settings'].has_key('pools')]:
-    #all_pools.update( value )
 
 random.seed(time.time())
 
+class DeadlineBundleSubmitter(dlm.DeadlineMayaSubmitter):
+
+    def __init__(self, name, project, episode, sequence, shot, *args, **kwargs):
+        super(DeadlineBundleSubmitter, self).__init__(*args, **kwargs)
+        self.chosen_pools = []
+        self.project_paths = []
+        self.name = name
+        self.project = project
+        self.episode = episode
+        self.sequence = sequence
+        self.shot = shot
+
+    def configure(self, sync=True):
+        self.conf = config.copy()
+
+        for override in self.conf.pop('overrides'):
+            match_all = bool(override.get('match_all', True))
+            applyOverride = False
+            conditions = override['conditions']
+            for cond in conditions:
+                try:
+                    operand1 = None
+                    operand2 = cond[1]
+                    method = 'eq'
+                    if len(cond) == 3:
+                        method = cond[1]
+                        operand2 = cond[2]
+                    if cond[0] == 'renderer':
+                        operand1 = imaya.currentRenderer()
+                    else:
+                        raise ValueError, 'Unknown condition'
+                    applyOverride = dl.matchValue(operand1, operand2, method)
+                    if (not applyOverride and match_all) or (applyOverride and
+                            not match_all):
+                        break
+                except Exception as e:
+                    print e
+
+            if applyOverride:
+                for key, value in override.get('settings', {}).items():
+                    self.conf[key]=value
+        self.validatePools()
+
+        if sync:
+            self.syncWithConf()
+
+        for pattern in self.conf.get('illegal_layer_names', []):
+            if re.match(pattern, str(self.currentLayer), re.I):
+                return False
+
+
+        for pattern in self.conf.get('illegal_camera_names', []):
+            if re.match(pattern, str(self.currentCamera), re.I):
+                return False
+
+        return True
+
+    def syncWithConf(self): 
+        chunkSize = self.conf.get("chunkSize")
+        if chunkSize is not None:
+            self.chunkSize = chunkSize
+
+        ignoreDefaultCamera = self.conf.get("ignoreDefaultCamera")
+        if ignoreDefaultCamera is not None:
+            self.ignoreDefaultCamera = ignoreDefaultCamera
+
+        priority = self.conf.get("priority")
+        if priority is not None:
+            self.priority = priority
+
+        submitAsSuspended = self.conf.get("submitAsSuspended")
+        if submitAsSuspended is not None:
+            self.submitAsSuspended = submitAsSuspended
+
+        submitEachCamera = self.conf.get("submitEachCamera")
+        if submitEachCamera is not None:
+            self.submitEachCamera = submitEachCamera
+
+        submitEachRenderLayer = self.conf.get("submitEachRenderLayer")
+        if submitEachRenderLayer is not None:
+            self.submitEachRenderLayer = submitEachRenderLayer
+
+        submitSceneFile = self.conf.get("submitSceneFile")
+        if submitSceneFile is not None:
+            self.submitSceneFile = submitSceneFile
+
+        self.pool = self.getPreferredPool()
+        self.bundle_base = self.getPreferredBase()
+
+        self.vardict = {
+            'bundle_base': self.bundle_base,
+            'project': self.project,
+            'episode': self.episode,
+            'sequence': self.sequence,
+            'shot': self.shot,
+            'name': self.name
+        }
+        self.bundle_loc = self.conf['bundle_loc'] % self.vardict
+        self.jobName = self.conf['jobname'] % self.vardict
+        self.outputPath = self.conf['output_loc']% self.vardict
+
+        self.projectPath = self.getNewProjectPath()
+        filename = os.path.basename(imaya.cmds.file(q=1, sn=1))
+        self.sceneFile = os.path.join( self.projectPath, "scenes", filename)
+
+    def createJobs(self):
+        self.chosen_pools = []
+        self.project_paths = []
+        super(DeadlineBundleSubmitter, self).createJobs()
+
+    def getNewProjectPath(self):
+        count = 0
+        projectPath = os.path.join( self.bundle_loc, self.name )
+
+        for chosen_path in self.project_paths:
+            if self.bundle_base in chosen_path:
+                return chosen_path
+
+        while os.path.exists(projectPath):
+            count += 1
+            projectPath = os.path.join( self.bundle_loc, "%s_%03d"%(self.name, count))
+        self.project_paths.append(projectPath)
+        return projectPath
+
+    def getPreferredBase(self):
+        bases = self.conf['pools'][self.pool].get('bases')
+        if not bases:
+            raise Exception, 'No basepaths for pool %s'%self.pool
+        for chosen_path in self.project_paths:
+            for base in bases:
+                if base in chosen_path:
+                    return base
+        return random.choice( bases )
+
+    def getPreferredPool(self):
+        pools = self.conf['pools']
+        method = self.conf.get('pool_selection')
+        for chosen_pool in self.chosen_pools:
+            if chosen_pool in pools.keys():
+                return chosen_pool
+        if method == 'min_frames_pending':
+            poolframes = self.getFramesPendingOnPools(pools.keys())
+            newpool = min(poolframes.keys(), key=lambda x:poolframes[x])
+            self.chosen_pools.append(newpool)
+            return newpool
+        else:
+            return random.choice(pools.keys())
+
+    def getFramesPendingInJob(self, job):
+        totalFrames = len(job["Frames"].split(","))
+        ratio = 1-float(job["CompletedChunks"])/int(job["TaskCount"])
+        return int(math.ceil(totalFrames * ratio))
+
+    def getFramesPendingOnPools(self, statuses = ["Queued", "Active"]):
+        if not hasattr(self, 'deadline_jobs'):
+            self.deadline_jobs = dl.getJobs()
+        jobs = self.deadline_jobs
+        jobs = dl.filterItems(jobs,
+                [("Status", status) for status in statuses] )
+        frames = dict()
+        for pool in self.conf['pools']:
+            pooljobs = dl.filterItems(jobs,[("PoolOverride", pool)])
+            frames[pool]= sum([self.getFramesPendingInJob(job) for job in pooljobs])
+        return frames
+
+    def validatePools(self):
+        pools = self.conf.get('pools', {})
+
+        if not hasattr(self, 'deadline_pools'):
+            self.deadline_pools = dl.pools()
+        deadline_pools = self.deadline_pools
+
+        valid_pools = {}
+        for pool, pool_settings in pools.items():
+            if pool not in deadline_pools:
+                continue
+
+            bases = pool_settings.get('bases', [])
+            valid_bases = []
+            for base in bases:
+                if base and os.path.exists(base) and os.path.isdir(base):
+                    valid_bases.append(base)
+
+            if valid_bases:
+                pool_settings['bases'] = valid_bases
+                valid_pools[pool] = pool_settings
+
+        if not valid_pools:
+            valid_pools = config['pools']
+
+        self.conf['pools'] = valid_pools
+
 all_pools = {}
-
-def getPreferredPool():
-    renderer = imaya.currentRenderer()
-    mypools = renderer_pools.get(renderer, renderer_pools['default'])
-
-    if renderer == 'redshift':
-        validPools = getValidPools(mypools)
-        if validPools:
-            poolframes = getFramesPendingOnPools(validPools)
-            return min(poolframes.keys(), key=lambda x:poolframes[x])
-
-    elif renderer == 'arnold':
-        validPools = getValidPools(mypools)
-        if validPools:
-            return random.choice(validPools)
-
-    return random.choice(mypools.keys())
 
 def createJobs(pool=None, outputPath=None, projectPath=None, sceneFile=None,
         jobName=None):
@@ -170,39 +342,4 @@ def detectBasepathFromProjectPath(projectPath, mypools=all_pools):
                 return path
     return projectPath
 
-def getBundleBase(pool):
-    base = all_pools[pool]
-    if not base:
-        raise Exception, 'No basepaths for pool %s'%pool
-    return random.choice(base)
-
-def getValidPools(mypools=all_pools):
-    validPools = []
-    for pool in dl.pools():
-        bases = mypools.get(pool)
-        if bases:
-            for base in bases:
-                if base and os.path.exists(base) and os.path.isdir(base):
-                    validPools.append(pool)
-                    break
-    return validPools
-
-def getFramesPendingInJob(job):
-    totalFrames = len(job["Frames"].split(","))
-    ratio = 1-float(job["CompletedChunks"])/int(job["TaskCount"])
-    return int(math.ceil(totalFrames * ratio))
-
-def getFramesPendingOnPools(pools,
-        statuses = ["Queued", "Active"]):
-    jobs = dl.getJobs()
-    jobs = dl.filterItems(jobs,
-            [("Status", status) for status in statuses] )
-    frames = dict()
-    for pool in pools:
-        pooljobs = dl.filterItems(jobs,[("PoolOverride", pool)])
-        frames[pool]= sum([getFramesPendingInJob(job) for job in pooljobs])
-    return frames
-
-if __name__ == '__main__':
-    print getFramesPendingOnPools(dl.pools())
 
